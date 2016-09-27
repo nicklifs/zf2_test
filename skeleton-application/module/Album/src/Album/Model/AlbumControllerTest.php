0eafb9fa18f2e33e02dcd4df87844d2bba1c05c7
nicklifs <nick.karbovsky@gmail.com>
2016-09-27 21:44:45 +0300

    init z2f app

---
+++ skeleton-application/module/Album/src/Album/Model/AlbumControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usr
+ * Date: 27.09.2016
+ * Time: 20:29
+ */
+
+/*public function testGetAlbumTableReturnsAnInstanceOfAlbumTable()
+{
+	$this->assertInstanceOf('Album\Model\AlbumTable', $this->controller->getAlbumTable());
+}*/
--- zf2_test/config/autoload/global.php
+++ zf2_test/config/autoload/global.php
@@ -11,18 +11,17 @@
  * file.
  */
 
-return [
+return array(
 	'db' => array(
 		'driver'         => 'Pdo',
 		'dsn'            => 'mysql:dbname=zf2_test;host=localhost',
 		'driver_options' => array(
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
-        ),
-    ),
-    'service_manager' => array(
-	'factories' => array(
-		'ZendDbAdapterAdapter'
-		=> 'Zend\Db\Adapter\AdapterServiceFactory',
+
+		),
 	),
-),
-];
+	'service_manager' => array(
+		'factories' => array(
+			'ZendDbAdapterAdapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+		),
+	),
+);
